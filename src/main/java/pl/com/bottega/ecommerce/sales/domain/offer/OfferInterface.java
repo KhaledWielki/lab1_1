261495f3615b3e8ecfcd002121a753c476af72b2
Adam Rezner 195047 <adam.rezner95@gmail.com>
2017-03-07 09:04:37 -0800

    Interface implementation

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferInterface.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/OfferInterface.java
@@ -3,6 +3,5 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 public interface OfferInterface {
 	
 	public int hashCode();
-	public boolean equals();
-	public boolean sameAs();
+	public boolean equals(Object obj);
 }
